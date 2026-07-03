c1e4a0e5bb92d3f67f3035a23774ad288ab475ba
imyoonsoo <bliss38soo@gmail.com>
2026-07-03 17:21:09 +0900

    docs: 문제 업데이트

---
+++ programmers/problems/L1/yellowlight.js
@@ -0,0 +1,4 @@
+/*
+ * [문제링크] https://school.programmers.co.kr/learn/courses/30/lessons/468371
+ * [문제] 노란불 신호등 (2025 카카오 하반기 1차)
+ */
---
+++ programmers/problems/L2/serverExpansions-number.js
@@ -0,0 +1,4 @@
+/*
+ * [문제링크] https://school.programmers.co.kr/learn/courses/30/lessons/389479
+ * [문제] 서버 증설 횟수 (2025 프로그래머스 코드챌린지 2차 예선)
+ */
