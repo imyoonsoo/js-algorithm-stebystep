42a75ea77c6fd872b558480c9f7e0e669cd139a2
imyoonsoo <bliss38soo@gmail.com>
2026-07-03 18:22:39 +0900

    docs: 문제 추가 수정

--- programmers/problems/L1/yellowlight.js
+++ programmers/problems/L1/yellowlight.js
@@ -1,4 +1,4 @@
 /*
- * [문제링크] https://school.programmers.co.kr/learn/courses/30/lessons/468371
- * [문제] 노란불 신호등 (2025 카카오 하반기 1차)
+ * 문제명: 노란불 신호등 (2025 카카오 하반기 1차)
+ * https://school.programmers.co.kr/learn/courses/30/lessons/468371
  */
